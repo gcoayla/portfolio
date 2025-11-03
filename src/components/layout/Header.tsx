9f3e698833d24508a80e8d556b21f03b80f82f63
gcoayla <gcoayla.28@gmail.com>
2025-11-03 02:30:35 -0500

    feat: added shadcn button

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Button } from "@/components/ui/button";
 
 export const Header = () => {
   return (
     <header className="sticky top-0 z-50 bg-background">
       <nav>
         <ul className="flex justify-center space-x-4">
-          <li><a href="#hero">Home</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#skills">Skills</a></li>
-          <li><a href="#contact">About / Contact</a></li>
+          <li>
+            <Button>
+              <a href="#hero">Home</a>
+            </Button>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#contact">About / Contact</a>
+          </li>
         </ul>
       </nav>
     </header>

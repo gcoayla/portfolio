46ee5e34fbf9f8521002de0bdaa105ca72af2219
gcoayla <gcoayla.28@gmail.com>
2025-11-04 22:47:21 -0500

    feat: added config for hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import { Typography } from "../ui/Typography";
 
 export const Hero = () => {
   return (
-    <section id="hero">
-      <h1>Your Name</h1>
-      <h2>Your Title</h2>
+    <section
+      id="hero"
+      className="h-lvh flex flex-col justify-center items-start gap-2"
+    >
+      <Typography variant="h1">Hi! I'm Gonzalo Coayla</Typography>
+      <Typography variant="h2">
+        Senior Frontend Engineer building polished, complex UIs—from interactive
+        3D models to scalable design systems.
+      </Typography>
     </section>
   );
 };

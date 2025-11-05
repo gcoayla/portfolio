acf52a2fbe3637ed3a787c630bea8a67f1c22945
gcoayla <gcoayla.28@gmail.com>
2025-11-05 00:40:22 -0500

    fix: polished hero component

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -4,10 +4,12 @@ export const Hero = () => {
   return (
     <section
       id="hero"
-      className="h-lvh flex flex-col justify-center items-start gap-2"
+      className="h-lvh pl-10 flex flex-col justify-center items-start gap-2"
     >
-      <Typography variant="h1">Hi! I'm Gonzalo Coayla</Typography>
-      <Typography variant="h2">
+      <Typography variant="h1" className="uppercase">
+        Hi! I'm Gonzalo Coayla
+      </Typography>
+      <Typography variant="h3" className="max-w=[80%] md:max-w-[70%]">
         Senior Frontend Engineer building polished, complex UIs—from interactive
         3D models to scalable design systems.
       </Typography>

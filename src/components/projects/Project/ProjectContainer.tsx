5157677fe5b584a3452ac151c6ccaff2ae3bcf51
gcoayla <gcoayla.28@gmail.com>
2025-11-12 04:46:36 -0500

    feat: updated typography responsive

--- src/components/projects/Project/ProjectContainer.tsx
+++ src/components/projects/Project/ProjectContainer.tsx
@@ -6,6 +6,8 @@ interface ProjectCardProps {
 
 export const ProjectContainer = ({ children }: ProjectCardProps) => {
   return (
-    <div className="w-full h-full rounded-lg shadow-lg p-8">{children}</div>
+    <div className="w-full h-full rounded-lg shadow-lg p-2 lg:p-8">
+      {children}
+    </div>
   );
 };
--- src/components/ui/Typography.tsx
+++ src/components/ui/Typography.tsx
@@ -18,12 +18,17 @@ export const Typography = ({
     {
       "font-heading": ["h1", "h2", "h3", "h4", "h5", "h6"].includes(variant),
       "font-body": ["p", "span"].includes(variant),
-      "text-5xl md:text-6xl font-bold leading-tight": variant === "h1",
-      "text-2xl font-bold leading-tight": variant === "h2",
-      "text-3xl font-bold leading-tight": variant === "h3",
-      "text-2xl md:text-2xl font-bold leading-tight": variant === "h4",
-      "text-xl font-semibold leading-normal": variant === "h5",
-      "text-lg font-semibold leading-normal": variant === "h6",
+      "text-3xl md:text-4xl lg:text-5xl font-bold leading-tight":
+        variant === "h1",
+      "text-2xl md:text-3xl lg:text-4xl font-bold leading-tight":
+        variant === "h2",
+      "text-xl md:text-2xl lg:text-3xl font-bold leading-tight":
+        variant === "h3",
+      "text-lg md:text-xl lg:text-2xl font-bold leading-tight":
+        variant === "h4",
+      "text-base md:text-lg lg:text-xl font-semibold leading-normal":
+        variant === "h5",
+      "text-base lg:text-lg font-semibold leading-normal": variant === "h6",
       "text-base font-normal leading-relaxed": variant === "p",
       "text-sm font-normal": variant === "span",
     },
